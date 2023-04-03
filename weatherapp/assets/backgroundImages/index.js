69a8342e1ce81cc9c8f5a4e12a486ab6be5759f4
Muhammad Rashid Khan <106462341+RashidYousufZai@users.noreply.github.com>
2023-04-03 13:47:36 +0500

    .

---
+++ weatherapp/assets/backgroundImages/index.js
@@ -0,0 +1,6 @@
+const haze = require('./img/1.jpg');
+const rainy = require('./img/2.jpg');
+const snow = require('./img/3.jpg');
+const sunny = require('./img/4.jpg');
+
+export { haze, rainy, snow, sunny };
